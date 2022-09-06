626ea70a93ac50dd589757e4d31c4bb022ac8636
dazedDive <dazed_dive@hotmail.fr>
2022-09-06 11:50:02 +0200

    api route

--- src/components/DistanceCalculator/DistanceCalculator.jsx
+++ src/components/DistanceCalculator/DistanceCalculator.jsx
@@ -8,9 +8,10 @@ const DistanceCalculator = ()=>{
     const [inputCpFact, setInputCpFact]=useState('')
     const [cityList, setCityList]=useState([])
     const [cityListFact, setCityListFact]=useState([])
-    
-
-   
+    const [cityLivraison, setCityLivraison]=useState([])
+    const [coordinates, Setcoordinates]=useState([])
+    const [location, setLocation]=useState([])
+    const [distance, setDistance]=useState()
 
     const HandleChange =(evt)=>{
         setInputCp(evt.target.value)
@@ -19,6 +20,40 @@ const DistanceCalculator = ()=>{
     const HandleChangeFact =(evt)=>{
         setInputCpFact(evt.target.value)
     }
+
+    const HandleSelected = (evt)=>{
+        setCityLivraison(evt.target.value)
+        console.log(evt.target.value)
+    }
+
+    useEffect(()=>{
+        const apiLocalisation = `https://geo.api.gouv.fr/communes?nom=${cityLivraison}&fields=centre`
+            fetch(apiLocalisation)
+            .then((rep)=>rep.json())
+            .then((rep)=>Setcoordinates(rep))
+            
+        },[cityLivraison])
+
+    useEffect(()=>{
+        if (cityLivraison){
+        const test=coordinates.find(city=>city.nom === cityLivraison )  
+        setLocation (test && test.centre.coordinates)}
+
+        console.log(location);
+        
+        if (location){
+        const livraisonLocation =location.toString() ;
+        console.log("test : " + (livraisonLocation));
+        const maretzLocation = "3.4079803,50.0523653";
+        const apiDriveDistance =`http://router.project-osrm.org/route/v1/driving/${maretzLocation};${livraisonLocation}?overview=false`;
+        fetch(apiDriveDistance)
+        .then((rep)=>rep.json())
+        .then((rep)=>setDistance(rep))}
+            },[coordinates])
+    
+
+
+
         useEffect(()=>{
         const apiCpUrl = `https://geo.api.gouv.fr/communes?codePostal=${inputCp}`
             fetch(apiCpUrl)
@@ -49,22 +84,22 @@ const DistanceCalculator = ()=>{
         <div className="row mt-5">
         <h1 className="text-resa ">Identité : </h1>
         <div className="col-6">
-        <input class="form-control" type="text" placeholder="Nom" 
+        <input className="form-control" type="text" placeholder="Nom" 
         aria-label="default input example"></input>
         </div>
         <div className="col-6">
-        <input class="form-control" type="text" placeholder="Prenom" 
+        <input className="form-control" type="text" placeholder="Prenom" 
         aria-label="default input example"></input>    
         </div>   
         </div>
         
         <div className="row mt-3">
         <div className="col-6">
-        <input class="form-control" type="text" placeholder="Telephone" 
+        <input className="form-control" type="text" placeholder="Telephone" 
         aria-label="default input example"></input>
         </div>
         <div className="col-6">
-        <input class="form-control" type="text" placeholder="Adresse Email" 
+        <input className="form-control" type="text" placeholder="Adresse Email" 
         aria-label="default input example"></input>    
         </div>   
         </div>
@@ -72,18 +107,18 @@ const DistanceCalculator = ()=>{
         <h1 className="text-resa mt-3">Adresse de Facturation :</h1>
         <div className="row mt-3">
         <div className="col-12">
-        <input class="form-control" type="text" placeholder="Adresse de Facturation" 
+        <input className="form-control" type="text" placeholder="Adresse de Facturation" 
         aria-label="default input example"></input>
         </div>
         </div>
 
         <div className="row mt-3">
         <div className="col-6">
-        <input class="form-control" type="text" placeholder="Code Postal de Facturation" 
+        <input className="form-control" type="text" placeholder="Code Postal de Facturation" 
         aria-label="default input example" onChange={HandleChangeFact}></input>
         </div>
         <div className="col-6">
-        <select class="form-select" aria-label="Default select example">
+        <select className="form-select" aria-label="Default select example">
         <option selected>Ville de Facturation</option>
         {cityListFact.map(({nom}) => <option value="1">{nom}</option>)}
         
@@ -96,27 +131,28 @@ const DistanceCalculator = ()=>{
         <h1 className="text-resa mt-3">Adresse de Livraison :</h1>
         <div className="row mt-3">
         <div className="col-12">
-        <input class="form-control" type="text" placeholder="Adresse de Livraison" 
+        <input className="form-control" type="text" placeholder="Adresse de Livraison" 
         aria-label="default input example"></input>
         </div>
         </div>
 
         <div className="row mt-3">
         <div className="col-6">
-        <input class="form-control" type="text" placeholder="Code Postal de Livraison" 
+        <input className="form-control" type="text" placeholder="Code Postal de Livraison" 
         aria-label="default input example" onChange={HandleChange}></input>
         </div>
         <div className="col-6">
-        <select class="form-select" aria-label="Default select example">
-        <option selected>Ville de Livraison</option>
-        {cityList.map(({nom}) => <option value="1">{nom}</option>)}
+        <select className="form-select" aria-label="Default select example" onChange={HandleSelected}>
+        <option selected >Ville de Livraison</option>
+        {cityList.map(({nom,code}) => <option key={code} value={nom}>{nom}</option>)}
+
         
         </select>   
         </div>
 
         </div>
         <span className="d-flex justify-content-end">
-        <button type="button" class="btn btn-light mt-4 w-25">Etape final</button>
+        <button type="button" className="btn btn-light mt-4 w-25 mb-5">Etape final</button>
         </span>
         </div>
         </>
