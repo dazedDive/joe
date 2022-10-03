47c7c181ed8898e2d5b1c156ede01187f4d3e77c
dazedDive <dazed_dive@hotmail.fr>
2022-10-03 16:17:06 +0200

    a debug

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -23,7 +23,7 @@ const Home =()=>{
         <>
         <div className="container mt-5">
         <h3 className='title-anim'>What's up?</h3>
-        <p className="orange">{homeText?.textHome}</p>
+        <h5 className="orange">{homeText?.textHome}</h5>
         </div>
         <div className="container-fluid mt-2 d-flex justify-content-center">
         
